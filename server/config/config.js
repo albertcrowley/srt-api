b395ef571570b5c03ec976bc54c429c7530fd6b4
Al Crowley <albert.crowley@tcg.com>
2018-12-13 15:03:26 -0500

    checkpoint trying typeorm

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,5 @@
 
+
 // Below two lines are the AWS Dev/Demo environment (developement Envirnoment)
 // TODO: add production to ENV configuration so we don't have to comment or uncomment lines
 // productionURL
@@ -8,6 +9,14 @@ process.env.PORT = 3000;
 // localdev
 process.env.MONGODB_URI = 'localhost:27017/Section508Dash';
 
+// process.env.POSTGRES_UIR = 'postgres://utz6kah1zq8t2nyg:of2fbobp423i89ztu7g7d136h@cg-aws-broker-prod-01fb6e1ed561.ci7nkegdizyy.us-gov-west-1.rds.amazonaws.com:5432/cgawsbrokerprodgq3wrvn8c8a772q';
+process.env.POSTGRES_UIR = 'postgres://utz6kah1zq8t2nyg:of2fbobp423i89ztu7g7d136h@192.168.1.171:5432/cgawsbrokerprodgq3wrvn8c8a772q';
+process.env.POSTGRES_DB = 'cgawsbrokerprodgq3wrvn8c8a772q';
+process.env.POSTGRES_PORT = '5432';
+process.env.POSTGRES_HOST = '192.168.1.171';
+process.env.POSTGRES_PASSWORD = 'of2fbobp423i89ztu7g7d136h';
+process.env.POSTGRES_USER = 'utz6kah1zq8t2nyg';
+
 // cloud.gov
-//process.env.MONGODB_URI = ZJJ01kWhWX:0NYmhvrxT7@xa2b82717666a4-master-client.service.kubernetes:31473/9rTP2N2npx
+// process.env.MONGODB_URI = ZJJ01kWhWX:0NYmhvrxT7@xa2b82717666a4-master-client.service.kubernetes:31473/9rTP2N2npx
 
