8a390966f95f023faa00d08610ee507670844397
Al Crowley <albert.crowley@tcg.com>
2019-01-04 12:34:18 -0500

    Added the login route. Fixed some bugs. Added logging.

--- server/config/config.js
+++ server/config/config.js
@@ -10,12 +10,12 @@ process.env.PORT = 3000;
 process.env.MONGODB_URI = 'localhost:27017/Section508Dash';
 
 // process.env.POSTGRES_UIR = 'postgres://utz6kah1zq8t2nyg:of2fbobp423i89ztu7g7d136h@cg-aws-broker-prod-01fb6e1ed561.ci7nkegdizyy.us-gov-west-1.rds.amazonaws.com:5432/cgawsbrokerprodgq3wrvn8c8a772q';
-process.env.POSTGRES_UIR = 'postgres://utz6kah1zq8t2nyg:of2fbobp423i89ztu7g7d136h@192.168.1.171:5432/cgawsbrokerprodgq3wrvn8c8a772q';
-process.env.POSTGRES_DB = 'cgawsbrokerprodgq3wrvn8c8a772q';
-process.env.POSTGRES_PORT = '5432';
-process.env.POSTGRES_HOST = '192.168.1.130';
-process.env.POSTGRES_PASSWORD = 'of2fbobp423i89ztu7g7d136h';
-process.env.POSTGRES_USER = 'utz6kah1zq8t2nyg';
+// process.env.POSTGRES_UIR = 'postgres://utz6kah1zq8t2nyg:of2fbobp423i89ztu7g7d136h@192.168.1.171:5432/cgawsbrokerprodgq3wrvn8c8a772q';
+// process.env.POSTGRES_DB = 'cgawsbrokerprodgq3wrvn8c8a772q';
+// process.env.POSTGRES_PORT = '5432';
+// process.env.POSTGRES_HOST = '192.168.1.130';
+// process.env.POSTGRES_PASSWORD = 'of2fbobp423i89ztu7g7d136h';
+// process.env.POSTGRES_USER = 'utz6kah1zq8t2nyg';
 
 // cloud.gov
 // process.env.MONGODB_URI = ZJJ01kWhWX:0NYmhvrxT7@xa2b82717666a4-master-client.service.kubernetes:31473/9rTP2N2npx
