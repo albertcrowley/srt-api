232dee85fdae9fd4f058e436bc9965f0e9041c53
Al Crowely <albert.crowley@tcg.com>
2019-09-03 12:07:58 -0400

    Adding better CORS logging

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const session = require('express-session')
 const CASAuthentication = require('cas-authentication')
 const jwtSecret = common.jwtSecret || undefined
 const {getConfig} = require('./config/configuration')
-const logger = require('../config/winston')
+const logger = require('./config/winston')
 
 if (! jwtSecret) {
   console.log("No JWT secret defined.  Be sure to set JWT_SECRET in the environment before running startup")
